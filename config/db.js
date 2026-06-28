ba70efb0305a26bb23da2746b305cbca42733507
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-28 20:33:42 +0330

    Add game model schema

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("DB Connected!");
+	} catch (error) {
+		console.log("DB Connection Error:", error.message);
+	}
+};
