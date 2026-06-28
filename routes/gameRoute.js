415c6076a75ff9a72f98c000f11019df201ac798
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-28 20:55:30 +0330

    Add POST Method

--- routes/gameRoute.js
+++ routes/gameRoute.js
@@ -4,5 +4,16 @@ import multer from "multer";
 
 const gameRouter = express.Router();
 
+// Image Storage Engine
+const storage = multer.diskStorage({
+	destination: "uploads/",
+	filename: (req, file, cb) => {
+		return cb(null, `${Date.now()}-${file.originalname}`);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+gameRouter.post("/add", upload.single("image"), addGame);
 
 export default gameRouter;
